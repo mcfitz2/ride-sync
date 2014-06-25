284da2858a8596ab8e5916bbc68ac6ad55f7e053
Micah Fitzgerald <mcfitz2@gmail.com>
2014-06-25 17:53:20 -0400

    fixed timezone bug

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ var fs        = require('fs')
 , path      = require('path')
 , Sequelize = require('sequelize')
 , lodash    = require('lodash')
-, sequelize = new Sequelize(process.env.DATABASE_URL, {native:true, ssl:true})
+, sequelize = new Sequelize(process.env.DATABASE_URL, {native:true, ssl:true, logging:false})
 , db        = {};
  
 fs
